e5bd86679671ce4d4a2f6b86b31286756bb326ef
avik1658 <avik.singha@brainstation-23.com>
2025-03-19 13:00:46 +0600

    Added login page

---
+++ app/login/page.tsx
@@ -0,0 +1,11 @@
+export default function LoginPage() {
+    return (
+        <div>
+            <form action="">
+                <input type="text" placeholder="Username" />
+                <input type="password" placeholder="Password" />
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
+}
